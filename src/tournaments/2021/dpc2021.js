678cf8dcb205e9690a8c74c4c4f36438a8ac8b82
Rhys Stever <rhys@stever.com>
2021-10-06 03:35:16 -0700

    Rearranged ti 10 teams based on offical group assignments

--- src/tournaments/2021/dpc2021.js
+++ src/tournaments/2021/dpc2021.js
@@ -26,30 +26,30 @@ function createTI() {
 		"https://liquipedia.net/dota2/The_International/2021", true, false);
 
 	// Add teams
-  tiGroupStageA.addTeam(new classes.Team("Evil Geniuses", "EG", 0, 0, 0, 0));
-  tiGroupStageA.addTeam(new classes.Team("Quincy Crew", "QC", 0, 0, 0, 0));
-  tiGroupStageA.addTeam(new classes.Team("Invictus Gaming", "iG", 0, 0, 0, 0));
-  tiGroupStageA.addTeam(new classes.Team("Team Secret", "Secret", 0, 0, 0, 0));
-  tiGroupStageA.addTeam(new classes.Team("Team Aster", "Aster", 0, 0, 0, 0));
   tiGroupStageA.addTeam(new classes.Team("Alliance", "[A]", 0, 0, 0, 0));
-  tiGroupStageA.addTeam(new classes.Team("SG e-sports", "SG", 0, 0, 0, 0));
-  tiGroupStageA.addTeam(new classes.Team("Team Spirit", "TSpirit", 0, 0, 0, 0));
-  tiGroupStageA.addTeam(new classes.Team("Elephant", "Elephant", 0, 0, 0, 0));
-
+  tiGroupStageA.addTeam(new classes.Team("Evil Geniuses", "EG", 0, 0, 0, 0));
+  tiGroupStageA.addTeam(new classes.Team("Invictus Gaming", "iG", 0, 0, 0, 0));
+  tiGroupStageA.addTeam(new classes.Team("OG", "OG", 0, 0, 0, 0));
+  tiGroupStageA.addTeam(new classes.Team("T1", "T1", 0, 0, 0, 0));
+  tiGroupStageA.addTeam(new classes.Team("Team Aster", "Aster", 0, 0, 0, 0));
+  tiGroupStageA.addTeam(new classes.Team("Undying", "UND", 0, 0, 0, 0));
+  tiGroupStageA.addTeam(new classes.Team("Thunder Predator", "TP", 0, 0, 0, 0));
+  tiGroupStageA.addTeam(new classes.Team("Virtus.pro", "VP", 0, 0, 0, 0));
+  
   // Group B
   let tiGroupStageB = new classes.Tournament(
-		"The International 10: Group B", "Group B", classes.regions.GLOBAL, "TI", "None", 
+    "The International 10: Group B", "Group B", classes.regions.GLOBAL, "TI", "None", 
 		"https://liquipedia.net/dota2/The_International/2021", true, false);
-  
+    
   // Add teams
-	tiGroupStageB.addTeam(new classes.Team("PSG.LGD", "PSG.LGD", 0, 0, 0, 0));
-  tiGroupStageB.addTeam(new classes.Team("Virtus.pro", "VP", 0, 0, 0, 0));
-  tiGroupStageB.addTeam(new classes.Team("T1", "T1", 0, 0, 0, 0));
+  tiGroupStageB.addTeam(new classes.Team("Elephant", "Elephant", 0, 0, 0, 0));
+  tiGroupStageB.addTeam(new classes.Team("Quincy Crew", "QC", 0, 0, 0, 0));
+  tiGroupStageB.addTeam(new classes.Team("Team Spirit", "TSpirit", 0, 0, 0, 0));
+  tiGroupStageB.addTeam(new classes.Team("Team Secret", "Secret", 0, 0, 0, 0));
+  tiGroupStageB.addTeam(new classes.Team("SG e-sports", "SG", 0, 0, 0, 0));
+  tiGroupStageB.addTeam(new classes.Team("PSG.LGD", "PSG.LGD", 0, 0, 0, 0));
   tiGroupStageB.addTeam(new classes.Team("Vici Gaming", "Vici", 0, 0, 0, 0));
   tiGroupStageB.addTeam(new classes.Team("beastcoast", "bc", 0, 0, 0, 0));
-  tiGroupStageB.addTeam(new classes.Team("Thunder Predator", "TP", 0, 0, 0, 0));
-  tiGroupStageB.addTeam(new classes.Team("Undying", "UND", 0, 0, 0, 0));
-  tiGroupStageB.addTeam(new classes.Team("OG", "OG", 0, 0, 0, 0));
   tiGroupStageB.addTeam(new classes.Team("Fnatic", "Fnatic", 0, 0, 0, 0));
 
 	// Add TI groups to the year
